d4bd5e485c85767ee8d04c3a9fd27b0b53473b5f
YeoSeungA <silo1116@naver.com>
2025-02-11 18:12:46 +0900

    v4_entity작성1

--- src/main/java/com/springboot/member/entity/Member.java
+++ src/main/java/com/springboot/member/entity/Member.java
@@ -1,4 +1,43 @@
 package com.springboot.member.entity;
 
+import com.springboot.question.entity.Question;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+// role을 추가한다. (권한)
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_SLEEP;
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동 중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
 }
--- src/main/java/com/springboot/question/entity/Question.java
+++ src/main/java/com/springboot/question/entity/Question.java
@@ -1,4 +1,59 @@
 package com.springboot.question.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+//    질문 등록될 때의 날짜 생성
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+//      질문 상태 값 질문 생성시 초기 상태값은 QUESTION_REGISTERED 이다.
+    @Enumerated(EnumType.STRING)
+    private QuestionStatus questionStatus = QuestionStatus.QUESTION_REGISTERED;
+//    공개 글의 여부
+    @Enumerated(EnumType.STRING)
+    private QuestionPublicStatus questionPublicStatus = QuestionPublicStatus.PUBLIC;
+
+    public enum QuestionStatus {
+        QUESTION_REGISTERED("질문 등록 상태"),
+        QUESTION_ANSWERED("답변 완료 상태"),
+        QUESTION_DELETED("질문 삭제 상태"),
+        QUESTION_DEACTIVED("질문 비솰성 상태");
+
+        @Getter
+        public String status;
+
+        QuestionStatus(String status) {
+            this.status = status;
+        }
+    }
+
+    public enum QuestionPublicStatus {
+        PUBLIC("공개글 상태"),
+        SECRET("비밀글 상태");
+
+        @Getter
+        public String status;
+
+        QuestionPublicStatus(String status) {
+            this.status = status;
+        }
+    }
 }
